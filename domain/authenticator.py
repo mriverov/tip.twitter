cc275a9956ca813e9aa4f39c1da05d5119eb2068
altamiranoErica <altamirano.erica@gmail.com>
2014-06-05 16:14:57 -0300

    Added key file and parser to read the file.

--- domain/authenticator.py
+++ domain/authenticator.py
@@ -1,13 +1,16 @@
+import ConfigParser
 import tweepy
 
 class Authenticator:
 
-    def __init__(self, apiKey, apiSecret, accessToken, accessTokenSecret):
-        self.consumer_key = apiKey
-        self.consumer_secret = apiSecret
-        self.access_key = accessToken
-        self.access_secret = accessTokenSecret
-
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read(['keys.cfg'])
+        self.consumer_key = config.get('API keys', 'apiKey')
+        self.consumer_secret = config.get('API keys', 'apiSecret')
+        self.access_key = config.get('API keys', 'accessToken')
+        self.access_secret = config.get('API keys', 'accessTokenSecret')
+        
     def authenticate(self):
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.set_access_token(self.access_key, self.access_secret)
--- domain/digger.py
+++ domain/digger.py
@@ -18,6 +18,6 @@ class Digger:
             stream.disconnect()
             
 if __name__ == "__main__":
-    a = authenticator.Authenticator('4EGWhOlbKIp8SIXjP56kRdxy8', '6j2XJDldCDFNfVM7Urr4Gddu2x1EJVxjSC9dRAdOd1r7KDfu0Z', '2463917743-oUidchs8WJT6zEPtp0d6fZCsVCslpyUisthyyem', 'feLQiY1bR3fOP85ZqNdZyI4h0fdmPJVkAybzbZvTg6408')
+    a = authenticator.Authenticator()
     d = Digger(a.authenticate())
     d.startStreaming()
