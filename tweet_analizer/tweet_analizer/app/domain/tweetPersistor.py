80c5a0461b9b34688e8f31ccf8c31c773436605d
mriverov <marina.riverov@gmail.com>
2014-08-17 00:16:52 -0300

    change folder name to backend

--- tweet_analizer/tweet_analizer/app/domain/tweetPersistor.py
+++
@@ -1,9 +0,0 @@
-from tweet_analizer.app.models import Tweet
-
-
-class TweetPersistor:
-
-    def saveTweet(self, atext, _id):
-        tweet = Tweet(text= atext, tweetid=_id)
-        tweet.save()
-        
