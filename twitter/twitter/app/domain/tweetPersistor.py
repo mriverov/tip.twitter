9055181ec0abd2202dc6f0129cf754189d27c25f
altamiranoErica <altamirano.erica@gmail.com>
2014-09-13 17:08:50 -0300

    Agregados parametros de las nuevas relaciones.

--- twitter/twitter/app/domain/tweetPersistor.py
+++ twitter/twitter/app/domain/tweetPersistor.py
@@ -7,10 +7,12 @@ from twitter.app.models import Tweet
 
 class TweetPersistor:
     #falta retweet_id
-    def saveTweet(self, _id, atext, _favorite_count,_retweet_count, user_id):
+    def saveTweet(self, _id, atext, _favorite_count,_retweet_count, _user, _retweet, _topic):
         _text = None        
         if atext is not None:
             _text = atext.encode('unicode_escape')
-        tweet = Tweet(tweetid =_id, text = _text,favorite_count=_favorite_count ,retweet_count=_retweet_count, user = user_id)
+        tweet = Tweet(tweetid =_id, text = _text,favorite_count=_favorite_count,
+                      retweet_count=_retweet_count, author = _user, retweet = _retweet,
+                      topic = _topic)
         tweet.save()
         
