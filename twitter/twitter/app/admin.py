db00e60127a62012cee713cbab81c1279816a774
mriverov <marina.riverov@gmail.com>
2014-09-14 22:24:13 -0300

    le cambie la parte de TweetUser por User, igualmente no esta actualizado

--- twitter/twitter/app/admin.py
+++ twitter/twitter/app/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from twitter.app.models import TweetUser
+from twitter.app.models import User
 from twitter.app.models import Tweet
 
 class TweetUserAdmin(admin.ModelAdmin):
 	list_display = ('name','description')
 
 
-admin.site.register(TweetUser,TweetUserAdmin)
+admin.site.register(User,TweetUserAdmin)
 
 class TweetTwitterAdmin(admin.ModelAdmin):
     list_display = ('tweet','description')
