dee0b42c5a3d6c5495a4a3848bae486768eb63e9
mriverov <marina.riverov@gmail.com>
2014-09-14 22:45:55 -0300

    clase que persiste un hashtag

---
+++ twitter/twitter/app/domain/hashtagPersistor.py
@@ -0,0 +1,18 @@
+from twitter.app.models import Hashtag
+
+class HashtagPersistor:
+    
+    def saveHashtag(self,hashtag_info, _topic, tweet):
+        hashtag = hashtag_info[0]
+        _name = hashtag['text']
+        if _name is not None:
+            _name = _name.encode('unicode_escape')
+        # TODO
+        # count
+        hashtag = Hashtag(name = _name, topic = _topic)
+        hashtag.save()
+        hashtag.tweets.add(tweet)
+        hashtag.save()
+        
+        
+    
