cf6bf141922d2a389e2bf2aa26de36eddf44684e
mriverov <marina.riverov@gmail.com>
2014-09-14 22:25:51 -0300

    Topic y Domain integrados para trackingKeys

--- twitter/twitter/app/domain/digger.py
+++ twitter/twitter/app/domain/digger.py
@@ -1,15 +1,17 @@
 import tweepy
-import authenticator
 import stream
+from twitter.app.domain.authenticator import Authenticator
+from twitter.app.domain.topicConfiguration import TopicConfiguration
 
 class Digger:
 
     def __init__(self, auth):
         self.auth = auth
         self.digger = stream.Stream()
+        self.topic = TopicConfiguration()
 
     def trackingKeys(self):
-        return ['#obama']
+        return self.getActualTopic()
 
     def startStreaming(self):   
         stream = tweepy.streaming.Stream(self.auth, self.digger)
@@ -20,9 +22,16 @@ class Digger:
         except Exception as e:
             print e
             stream.disconnect()
+    
+    def getActualTopic(self):
+        key = []
+        key.append(self.topic.getTopicLikeHashtag())
+        return key
+        
 
             
 if __name__ == "__main__":
-    a = authenticator.Authenticator()
+    a = Authenticator()
     d = Digger(a.authenticate())
+    d.topic.saveConfiguration("Deporte","premierLeague") 
     d.startStreaming()
