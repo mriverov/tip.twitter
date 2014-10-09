3c1922ba9951539aa960db5e11e82479cc450dcf
altamiranoErica <altamirano.erica@gmail.com>
2014-10-09 17:29:27 -0300

    Guarda el usuario autor del retweet + refactor.

--- twitter/twitter/app/domain/tweetPersistor.py
+++ twitter/twitter/app/domain/tweetPersistor.py
@@ -3,51 +3,54 @@ from twitter.app.models import Tweet
 class TweetPersistor:
     
     def saveTweet(self,content, _topic, user, user_persistor):
-        _retweet_id = None
-        _retweet = None   
+        tweet = self.getTweetObject(content, _topic, user)
+        user_mentions = user_persistor.getUserMention(content['entities']['user_mentions'])
+        retweet = self.saveRetweet(content, _topic, user_persistor)
         
-        _tweet = self.getTweetObject(content, _topic, user)
-        _user_mentions = user_persistor.getUserMention(content['entities']['user_mentions'])
-        _retweet = self.saveRetweet(content, _topic, user, user_persistor, _retweet_id)
+        tweet.save()
+        tweet.retweet = retweet
+        if(user_mentions is not None):
+            tweet.user_mentions.add(user_mentions)
+        tweet.save()
         
-        _tweet.save()
-        _tweet.retweet = _retweet
-        if(_user_mentions is not None):
-            _tweet.user_mentions.add(_user_mentions)
-        _tweet.save()
-        
-        return _tweet
+        return tweet
     
-    def saveRetweet(self, content, _topic, user, user_persistor, _retweet_id):
-        _retweet = None
+    def saveRetweet(self, content, _topic, user_persistor):
+        retweet = None
         if 'retweeted_status' in content:
-            _retweeted_status = content['retweeted_status']
-            if (_retweeted_status is not None):
-                _retweet_id = _retweeted_status['id']
+            retweeted_status = content['retweeted_status']
+            if (retweeted_status is not None):
+                retweet_id = retweeted_status['id']
                 try:
-                    _retweet = Tweet.objects.get(tweetid=_retweet_id)
+                    retweet = Tweet.objects.get(tweetid = retweet_id)
                 except Tweet.DoesNotExist:
-                    _retweet = self.getTweetObject(_retweeted_status, _topic, user)
-                    _retweet.save()
-                    _retweet.retweet = None
-                    _user_mention = user_persistor.getUserMention(_retweeted_status['entities']['user_mentions'])
-                    if _user_mention is not None:
-                        _retweet.user_mentions.add(_user_mention)
-                    _retweet.save()
-        return _retweet
+                    user = user_persistor.saveUserWithoutFollowers(retweeted_status['user'])
+                    retweet = self.getTweetObject(retweeted_status, _topic, user)
+                    retweet.save()
+                    retweet.retweet = None
+                    user_mention = user_persistor.getUserMention(retweeted_status['entities']['user_mentions'])
+                    if user_mention is not None:
+                        retweet.user_mentions.add(user_mention)
+                    retweet.save()
+        return retweet
 
     
     def getTweetObject(self, content, _topic, _user):
-        _text = content['text'] 
+        tweet = None
         _id = content['id']
         _favorite_count = content['favorite_count']
         _retweet_count = content['retweet_count']
-        _user = _user
+        
+        _text = content['text'] 
+        
         if _text is not None:
             _text = _text.encode('unicode_escape')
         
-        tweet = Tweet(tweetid =_id, text = _text, favorite_count=_favorite_count, 
-                  retweet_count=_retweet_count, author = _user, topic = _topic)
+        try:
+            tweet = Tweet.objects.get(tweetid = _id)
+        except Tweet.DoesNotExist:
+            tweet = Tweet(tweetid =_id, text = _text, favorite_count=_favorite_count, 
+                          retweet_count =_retweet_count, author = _user, topic = _topic)
         
         return tweet
           
