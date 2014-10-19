4123cf720f1e634b7ddf07a2ea9dd35cf8998f6c
mriverov <marina.riverov@gmail.com>
2014-10-19 17:31:37 -0300

    metodos de cada pantalla de configuracion

--- twitter/twitter/app/views.py
+++ twitter/twitter/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+#from django.core.context_processors import csrf
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("Hello world")
+
+
+def getHome(request):
+    return render(request, 'home.html')
+
+
+def newProject(request):
+    domain = request.POST['project']
+      
+
+    return render_to_response('configuration.html', {'domain':domain})
