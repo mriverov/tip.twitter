70598e566df3d8fa1c7a00657a58df1e38051455
mriverov <marina.riverov@gmail.com>
2014-10-19 17:27:08 -0300

    borre lo que habia porque no funcionaba

--- twitter/twitter/app/admin.py
+++ twitter/twitter/app/admin.py
@@ -2,17 +2,4 @@ from django.contrib import admin
 
 # Register your models here.
 
-from twitter.app.models import User
-from twitter.app.models import Tweet
-
-class TweetUserAdmin(admin.ModelAdmin):
-	list_display = ('name','description')
-
-
-admin.site.register(User,TweetUserAdmin)
-
-class TweetTwitterAdmin(admin.ModelAdmin):
-    list_display = ('tweet','description')
-
-admin.site.register(Tweet,TweetTwitterAdmin)
 
